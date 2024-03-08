4bbf159f9c54e370910949ed0ca5e4f219a20549
CJL <using_namespace_std@outlook.com>
2024-03-08 20:30:15 +0800

    update uart16550 and logger.rs, successfully start YSOS

--- pkg/kernel/src/drivers/uart16550.rs
+++ pkg/kernel/src/drivers/uart16550.rs
@@ -1,27 +1,82 @@
 use core::fmt;
-
+use x86_64::instructions::port::{Port, PortReadOnly, PortWriteOnly};
 /// A port-mapped UART 16550 serial interface.
-pub struct SerialPort;
+//Fix: struct
+pub struct SerialPort{
+    data: Port<u8>,
+    interrupt_enable: PortWriteOnly<u8>,
+    interrupt_identification: PortWriteOnly<u8>,
+    line_control: PortWriteOnly<u8>,
+    modem_control: PortWriteOnly<u8>,
+    line_status: PortReadOnly<u8>,
+}
 
 impl SerialPort {
     pub const fn new(port: u16) -> Self {
-        Self
+        //FIX: init
+        Self{
+            data: Port::new(port + 0),
+            interrupt_enable: PortWriteOnly::new(port + 1),
+            interrupt_identification: PortWriteOnly::new(port + 2),
+            line_control: PortWriteOnly::new(port + 3),
+            modem_control: PortWriteOnly::new(port + 4),
+            line_status: PortReadOnly::new(port + 5),
+        }
     }
 
     /// Initializes the serial port.
-    pub fn init(&self) {
+    pub fn init(&mut self) {//这里原来是不可变借用，改成了可变借用
         // FIXME: Initialize the serial port
+        unsafe{
+            // Disable all interrupts
+            self.interrupt_enable.write(0x00);
+            // Enable DLAB (set baud rate divisor)
+            self.line_control.write(0x80);
+            // Set divisor to 3 (lo byte) 38400 baud
+            self.data.write(0x03);
+            // Set divisor to 3 (hi byte) 38400 baud
+            self.interrupt_enable.write(0x00);
+            // 8 bits, no parity, one stop bit
+            self.line_control.write(0x03);
+            // Enable FIFO, clear them, with 14-byte threshold
+            self.interrupt_identification.write(0xc7);
+            // IRQs enabled, RTS/DSR set
+            self.modem_control.write(0x0B);
+            // Set in loopback mode, test the serial chip
+            self.modem_control.write(0x1e);
+            // Test serial chip (send byte 0xAE and check if serial returns same byte)
+            self.data.write(0xae);
+            // Check if serial is faulty (i.e: not same byte as sent)
+            if self.data.read()!= 0xae{
+                panic!("serial is faulty");
+            }
+            // If serial is not faulty set it in normal operation mode
+            // (not-loopback with IRQs enabled and OUT#1 and OUT#2 bits enabled)
+            self.modem_control.write(0x0f);
+
+        }
     }
 
     /// Sends a byte on the serial port.
     pub fn send(&mut self, data: u8) {
         // FIXME: Send a byte on the serial port
+        unsafe{
+            while self.line_status.read() & 0x20 ==0{}
+            self.data.write(data);
+        }
     }
 
     /// Receives a byte on the serial port no wait.
     pub fn receive(&mut self) -> Option<u8> {
         // FIXME: Receive a byte on the serial port no wait
-        None
+        unsafe{
+            if self.line_status.read() & 1 ==0{
+                Some(self.data.read())
+            }else{
+                None
+            }
+            
+        }
     }
 }
 
--- pkg/kernel/src/utils/logger.rs
+++ pkg/kernel/src/utils/logger.rs
@@ -1,10 +1,12 @@
-use log::{Metadata, Record};
+use log::{Metadata, Record, Level, LevelFilter};
 
 pub fn init() {
     static LOGGER: Logger = Logger;
     log::set_logger(&LOGGER).unwrap();
 
     // FIXME: Configure the logger
+    //设置最高输出等级为Trace，以便查看所有log的效果
+    log::set_max_level(LevelFilter::Trace);
 
     info!("Logger Initialized.");
 }
@@ -18,6 +20,41 @@ impl log::Log for Logger {
 
     fn log(&self, record: &Record) {
         // FIXME: Implement the logger with serial output
+        if self.enabled(record.metadata()) {
+            // println!("{} - {}", record.level(), record.args());
+            match record.level() {
+                Level::Error => println!(
+                    "\x1b[31;1;4m[X] Error\x1b[0m  - \x1b[31mfrom {}, at line {}, {}\x1b[0m",
+                    record.file_static().unwrap(),
+                    record.line().unwrap(),
+                    record.args(),
+                ),
+                Level::Warn => println!(
+                    "\x1b[33;1;4m[!] Warning\x1b[0m- \x1b[33mfrom {}, at line {}, {}\x1b[0m",
+                    record.file_static().unwrap(),
+                    record.line().unwrap(),
+                    record.args(),
+                ),
+                Level::Info => println!(
+                    "\x1b[34;1;4m[+] Info\x1b[0m   - \x1b[34mfrom {}, at line {}, {}\x1b[0m",
+                    record.file_static().unwrap(),
+                    record.line().unwrap(),
+                    record.args(),
+                ),
+                Level::Debug => println!(
+                    "\x1b[36;1;4m[#] Debug\x1b[0m  - \x1b[36mfrom {}, at line {}, {}\x1b[0m",
+                    record.file_static().unwrap(),
+                    record.line().unwrap(),
+                    record.args(),
+                ),
+                Level::Trace => println!(
+                    "\x1b[32;1;4m[%] Trace\x1b[0m  - \x1b[32mfrom {}, at line {}, {}\x1b[0m",
+                    record.file_static().unwrap(),
+                    record.line().unwrap(),
+                    record.args(),
+                ),
+            }
+        }
     }
 
     fn flush(&self) {}
