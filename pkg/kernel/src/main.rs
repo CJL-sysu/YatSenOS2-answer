4304d3de3b6b72df7d60826f42ef65eb0ed42c16
CJL <using_namespace_std@outlook.com>
2024-03-30 11:10:53 +0800

    init lab4

--- pkg/kernel/src/main.rs
+++ pkg/kernel/src/main.rs
@@ -3,31 +3,38 @@
 
 use ysos::*;
 use ysos_kernel as ysos;
-use crate::drivers::input;
-use crate::interrupt::clock::read_counter;
+
 extern crate alloc;
 
 boot::entry_point!(kernel_main);
 
 pub fn kernel_main(boot_info: &'static boot::BootInfo) -> ! {
     ysos::init(boot_info);
-    // unsafe{
-    //     // let a:*mut u8 = 0x1111111111111 as *mut u8;
-    //     // *a = 99;
-    //     let a:*mut u8 = 0x1000000000 as *mut u8;
-    //     let b:u8 = *a + 1;
-    //     println!("{b}");
-    // }
-    loop {
-        print!("> ");
-        let input = input::get_line();
 
-        match input.trim() {
+    // FIXME: update lib.rs to pass following tests
+
+    // 1. run some (about 5) "test", show these threads are running concurrently
+
+    // 2. run "stack", create a huge stack, handle page fault properly
+
+    let mut test_num = 0;
+
+    loop {
+        print!("[>] ");
+        let line = input::get_line();
+        match line.trim() {
             "exit" => break,
-            _ => {
-                println!("You said: {}", input);
-                println!("The counter value is {}", read_counter());
+            "ps" => {
+                ysos::proc::print_process_list();
             }
+            "stack" => {
+                ysos::new_stack_test_thread();
+            }
+            "test" => {
+                ysos::new_test_thread(format!("{}", test_num).as_str());
+                test_num += 1;
+            }
+            _ => println!("[=] {}", line),
         }
     }
 
